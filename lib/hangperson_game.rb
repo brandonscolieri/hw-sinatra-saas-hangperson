92ecd1d682689d827f58448747e21e4ae6c04ce2
Brandon Scolieri <brandonscolieri@users.noreply.github.com>
2019-09-29 22:09:41 -0700

    game logic complete

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,15 @@ class HangpersonGame
 
   # def initialize()
   # end
+
+  attr_accessor :word, :guesses, :wrong_guesses, :guess, :allGuesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guess = ""
+    @guesses = ""
+    @wrong_guesses = ""
+    @allGuesses = Set.new
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -23,5 +29,102 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+
+
+  def hasGuessed?
+    if isValidGuess?(@guess)
+      recentGuessSet = @guess.split("").to_set
+      collisions = @allGuesses & recentGuessSet
+      if collisions.empty?
+        return false
+      else
+        return true
+      end
+    else
+      return "Invalid Input"
+    end
+  end
+
+
+  def guess userGuess
+    @guess = userGuess.to_s.downcase
+    self.updateGameState
+  end
+
+
+  def updateGameState
+    if isValidGuess?(@guess)
+      if hasGuessed?
+        return false
+      else
+        @allGuesses.add(@guess)
+        if isCorrectGuess?
+          puts "Correct!"
+          @guesses += @guess
+        else
+          puts "Incorrect Guess!"
+          @wrong_guesses += @guess
+        end
+      end
+    end
+  end
+
+
+  def isCorrectGuess?
+    if isValidGuess?(@guess)
+      recentGuessSet = @guess.split("").to_set
+      secretWord = @word.split("").to_set
+      collisions = secretWord & recentGuessSet
+      if collisions.empty?
+        return false
+      else
+        return true
+      end
+    end
+  end
+
+
+  def isValidGuess? guess
+    if isValidGuessHelper? guess
+      return true
+    else
+      raise ArgumentError.new("Your guess, #{@guess}, is not valid. Please enter a letter of the alphabet.")
+    end
+  end
+
+
+  def isValidGuessHelper? guess
+    alphabet = ("a".."z").to_a
+    guess = @guess
+    alphabet.each do |char|
+      if guess[0] == char
+        return true
+      end
+    end
+    return false
+  end
+
+
+  def word_with_guesses
+    if @guesses.empty? == false
+      @word.gsub(/[^#{@guesses}]/, '-')
+    else
+      @word.gsub(/./, '-')
+    end
+  end
+
+
+  def check_win_or_lose
+    secretWord = @word.split("").to_set
+    attempts = @guesses.split("").to_set
+    if @wrong_guesses.length >= 7
+      return :lose
+    elsif (attempts == secretWord) & (@wrong_guesses.length < 7)
+      return :win
+    else
+      return :play
+    end
+  end
+
 
 end
